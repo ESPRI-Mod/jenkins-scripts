65ef85c92520a0dc90fc3ac8ee9130c53ae51003
Sébastien Gardoll <sebastien@gardoll.fr>
2018-05-16 11:03:55 +0200

    refactore the function display

--- autoupdate_plugins_job.sh
+++ autoupdate_plugins_job.sh
@@ -6,11 +6,14 @@ set -o pipefail
 
 readonly WORKSPACE_PATH="${WORKSPACE}" # Set by Jenkins
 readonly COMMON_DIR_PATH="${JENKINS_HOME}/esgf" # Set by Jenkins
-readonly SCRIPT_FILE_PATH="${WORKSPACE_PATH}/jenkins-scripts/autoupdate_plugins_job.sh"
+readonly SCRIPT_DIR_PATH="${WORKSPACE_PATH}/jenkins-scripts"
+readonly SCRIPT_FILE_PATH="${SCRIPT_DIR_PATH}/autoupdate_plugins_job.sh"
 
 readonly CREDENTIAL_FILE_PATH="${COMMON_DIR_PATH}/.ipslbuild_credentials.secret"
 readonly JENKINS_JAR_PATH="${WORKSPACE_PATH}/jenkins-cli.jar"
 readonly JENKINS_SERVER_URL='https://localhost:8443/jenkins'
+
+source "${SCRIPT_DIR_PATH/common}"
 
 function execute_cmd
 {
@@ -19,21 +22,22 @@ function execute_cmd
 
 function main
 {
-  echo "> download jenkins-cli.jar"
+  display 'downloading jenkins-cli.jar' 'info'
   # Always download the last version of jenkins-cli.jar .
   wget --no-check-certificate -q -O "${JENKINS_JAR_PATH}" https://localhost:8443/jenkins/jnlpJars/jenkins-cli.jar
-  echo "> fetching the list of plugins."
+  display 'fetching the list of plugins.' 'info'
   set +u
   set +e
   plugin_list=$(execute_cmd 'list-plugins' 2> /dev/null | grep -P '\(.+\)$' | awk '{print $1}' | tr '\n' ' ')
   if [ -z "${plugin_list}" ]; then
-    echo "> nothing to update."
+    display 'nothing to update.' 'info'
     exit 0
   else
     set -u
     set -e
-    echo "> upgrading: ${plugin_list[*]}."
+    display "upgrading: ${plugin_list[*]}." 'info'
     execute_cmd "install-plugin ${plugin_list}"
+    display 'restarting Jenkins.' 'info'
     nohup sh -c "source \"${SCRIPT_FILE_PATH}\" ; execute_cmd 'safe-restart'" > /dev/null 2>&1 &
     exit $?
   fi
