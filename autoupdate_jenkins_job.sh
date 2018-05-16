5eae29531d20cf71d2d20ed4bf5d122b2ec0fdf3
Sébastien Gardoll <sebastien@gardoll.fr>
2018-05-16 14:30:10 +0200

    fix subprocess killing by Jenkins

--- autoupdate_jenkins_job.sh
+++ autoupdate_jenkins_job.sh
@@ -9,6 +9,7 @@ readonly JENKINS_HASH_URL="${BASE_URL}/jenkins.war.sha256"
 readonly JENKINS_WAR_URL="${BASE_URL}/jenkins.war"
 
 readonly WORKSPACE_PATH="${WORKSPACE}" # Set by Jenkins
+readonly SCRIPT_DIR_PATH="${WORKSPACE_PATH}/jenkins-scripts"
 
 readonly WEBAPP_DIR_PATH='/usr/share/tomcat/webapps'
 readonly JENKINS_WAR_FILENAME='jenkins.war'
@@ -17,27 +18,30 @@ readonly JENKINS_WAR_FILE_PATH="${WEBAPP_DIR_PATH}/${JENKINS_WAR_FILENAME}"
 
 readonly BACKUP_DIR_PATH="${WORKSPACE_PATH}/jenkins_old_version"
 
+source "${SCRIPT_DIR_PATH}/common"
+
 cd "${WEBAPP_DIR_PATH}"
 
-echo "> comparing the latest version of Jenkins with the installed one."
+display 'comparing the latest version of Jenkins with the installed one.' 'info'
 set +e
 wget -q -O - "${JENKINS_HASH_URL}" | sha256sum -c > /dev/null 2>&1
 returned_code=$?
 set -e
 
 if [ $returned_code -ne 0 ]; then
-  echo "> downloading a new version of Jenkins."
+  display 'downloading a new version of Jenkins.' 'info'
   wget -q -O "${JENKINS_WAR_TMP_FILE_PATH}" "${JENKINS_WAR_URL}"
-  echo "> backup Jenkins to ${BACKUP_DIR_PATH}."
+  display "backup Jenkins to ${BACKUP_DIR_PATH}." 'info'
   mkdir -p "${BACKUP_DIR_PATH}"
   cp -fp "${JENKINS_WAR_FILE_PATH}" "${BACKUP_DIR_PATH}"
-  echo "> delete the previous Jenkins installation."
+  display 'delete the previous Jenkins installation.' 'info'
   rm -fr "${JENKINS_WAR_FILE_PATH}" "$(basename -s '.war' ${JENKINS_WAR_FILE_PATH})"
-  echo "> run a nohup sequence of commands."
+  display 'reinstall Jenkins (nohup)'
   date >> "${BACKUP_DIR_PATH}/nohup.log"
-  nohup sh -c "sleep 20 ; mv -f ${JENKINS_WAR_TMP_FILE_PATH} ${JENKINS_WAR_FILE_PATH}" >> "${BACKUP_DIR_PATH}/nohup.log" 2>&1 &
+  # The BUILD_ID=whatever is the trick that cancel Jenkins to kill the processes spawned by the job.
+  BUILD_ID=dontKillMe nohup bash -c "sleep 10 ; mv -f ${JENKINS_WAR_TMP_FILE_PATH} ${JENKINS_WAR_FILE_PATH}" >> "${BACKUP_DIR_PATH}/nohup.log" 2>&1 &
   exit 0
 else
-  echo "> current Jenkins is up to date: nothing to do."
+  display 'current Jenkins is up to date: nothing to do.' 'info'
   exit 0
 fi
